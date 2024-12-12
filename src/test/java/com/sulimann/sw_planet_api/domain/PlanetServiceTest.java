912137cc2b0d6b5acf4a3951f63fc206e2f7b610
Rafael Sulimann <sulimannrafael@gmail.com>
2024-12-11 22:41:02 -0300

    Update unit test without @SpringBootTest

--- src/test/java/com/sulimann/sw_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/com/sulimann/sw_planet_api/domain/PlanetServiceTest.java
@@ -1,21 +1,22 @@
 package com.sulimann.sw_planet_api.domain;
 
 import static com.sulimann.sw_planet_api.common.PlanetConstants.PLANET;
-import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
-  @Autowired
+  @InjectMocks
   private PlanetService planetService;
 
-  @MockitoBean
+  @Mock
   private PlanetRepository planetRepository;
 
   @Test
